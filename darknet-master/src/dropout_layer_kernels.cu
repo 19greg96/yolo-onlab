c2978a59b385b5066d9d3cc1dac59aca9f14f6d1
greg1996 <greg.karz@gmail.com>
2019-05-04 15:00:58 +0200

    .

--- darknet-master/src/dropout_layer_kernels.cu
+++ darknet-master/src/dropout_layer_kernels.cu
@@ -8,10 +8,10 @@ extern "C" {
 #include "utils.h"
 }
 
-__global__ void yoloswag420blazeit360noscope(float *input, int size, float *rand, float prob, float scale)
+__global__ void yoloswag420blazeit360noscope(float *input, float* output, int size, float *rand, float prob, float scale)
 {
     int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-    if(id < size) input[id] = (rand[id] < prob) ? 0 : input[id]*scale;
+    if(id < size) output[id] = (rand[id] < prob) ? 0 : input[id]*scale;
 }
 
 void forward_dropout_layer_gpu(dropout_layer layer, network_state state)
@@ -27,7 +27,7 @@ void forward_dropout_layer_gpu(dropout_layer layer, network_state state)
     cuda_push_array(layer.rand_gpu, layer.rand, size);
     */
 
-    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK, 0, get_cuda_stream() >>>(state.input, size, layer.rand_gpu, layer.probability, layer.scale);
+    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK, 0, get_cuda_stream() >>>(state.input, layer.output_gpu, size, layer.rand_gpu, layer.probability, layer.scale);
     CHECK_CUDA(cudaPeekAtLastError());
 }
 
@@ -36,6 +36,6 @@ void backward_dropout_layer_gpu(dropout_layer layer, network_state state)
     if(!state.delta) return;
     int size = layer.inputs*layer.batch;
 
-    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK, 0, get_cuda_stream() >>>(state.delta, size, layer.rand_gpu, layer.probability, layer.scale);
+    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK, 0, get_cuda_stream() >>>(state.delta, layer.delta_gpu, size, layer.rand_gpu, layer.probability, layer.scale);
     CHECK_CUDA(cudaPeekAtLastError());
 }
